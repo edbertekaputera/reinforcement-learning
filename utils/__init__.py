cac3955a671169ee606a1cdc89e384af46e22491
jovanjoto <jovan_n@mail.com>
2024-08-21 01:43:40 +0800

    added qlearning

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,7 @@
 from .dqn_trainer import DQNTrainer
 from .tester import Tester
+from .QLearning_trainer import QLearningTrainer
 
 __all__ = [
-	"DQNTrainer", "Tester"
+	"DQNTrainer", "Tester", "QLearningTrainer"
 ]
